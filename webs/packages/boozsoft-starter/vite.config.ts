470e6d7a80aa627931a45dad89e04df4eb6d381c
{cheng} <321841642@qq.com>
2022-11-04 09:36:50 +0800

    chore(reform dir): reform dir

--- webs/packages/boozsoft-starter/vite.config.ts
+++ webs/packages/boozsoft-starter/vite.config.ts
@@ -2,12 +2,11 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base:'/nc',
-  plugins: [vue(), vueJsx()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
