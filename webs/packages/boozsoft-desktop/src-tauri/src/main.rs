a46fbc899ec559223f43fb11b192dc6f1e51e2ea
{cheng} <321841642@qq.com>
2022-11-08 17:44:34 +0800

    reform(reform dir): reform dir

--- webs/packages/boozsoft-desktop/src-tauri/src/main.rs
+++ webs/packages/boozsoft-desktop/src-tauri/src/main.rs
@@ -2,16 +2,33 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-
+use std::io::Write;
+use std::{fs, io,env, path::Path};
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+use std::io::Read;
 #[tauri::command]
 fn greet(name: &str) -> String {
+     let mut file = std::fs::File::create("data.txt").expect("create failed");
+       file.write_all(name.as_bytes()).expect("write failed");
+       println!("data written to file" );
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+#[tauri::command]
+fn asdsadas(name: &str) -> String {
+   let mut file = std::fs::File::open("data.txt").unwrap();
+     let mut contents = String::new();
+     file.read_to_string(&mut contents).unwrap();
+     print!("{}", contents);
+     format!("{}",contents )
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+                                                      asdsadas,
+                                                      greet
+                                                    ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- webs/packages/boozsoft-desktop/src/components/Greet.vue
+++ webs/packages/boozsoft-desktop/src/components/Greet.vue
@@ -1,20 +1,52 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/tauri";
+import {onMounted, ref} from "vue";
+import {invoke} from "@tauri-apps/api/tauri";
+
+import {
+  WebviewWindow,
+  WebviewWindowHandle,
+  WindowManager,
+  CloseRequestedEvent,
+  getCurrent,
+  getAll,
+  appWindow,
+  LogicalSize,
+  PhysicalSize,
+  LogicalPosition,
+  PhysicalPosition,
+  UserAttentionType,
+  currentMonitor,
+  primaryMonitor,
+  availableMonitors
+} from '@tauri-apps/api/window'
 
 const greetMsg = ref("");
-const name = ref("");
+const name = ref("127.0.0.1:3000");
+
+async function getAddr() {
+  return await invoke("asdsadas", {name: name.value});
+}
+
+;(async function(){
+  const addr = await getAddr()
+  if (addr != null) {
+    console.log("http://"+addr)
+    window.location.href = "http://"+addr+"/nc"
+  }
+})();
 
 async function greet() {
+  await invoke("asdsadas", {name: name.value});
+  window.location.href = name.value
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsg.value = await invoke("greet", { name: name.value });
+
 }
 </script>
 
 <template>
   <div class="card">
-    <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-    <button type="button" @click="greet()">Greet</button>
+    <input id="greet-input" v-model="name" placeholder="Enter a name..."/>
+    <button type="button" @click="greet()">开始</button>
   </div>
 
   <p>{{ greetMsg }}</p>
