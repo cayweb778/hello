6665b281ba41c18ba874e3742c222dbdc42caa64
{cheng} <321841642@qq.com>
2022-11-09 16:14:42 +0800

    `reform(reform dir): reform dir`

---
+++ webs/packages/boozsoft-desktop/src/router/guard/desktopGuard.ts
@@ -0,0 +1,29 @@
+import type { Router } from 'vue-router';
+
+
+
+import {getAddr} from "../../funs";
+import {useDesktopStoreWidthOut} from "../../store/modules/hello";
+
+
+
+export function createDesktopGuard(router: Router) {
+  router.beforeEach(async (to, from, next) => {
+
+    // Jump to the 404 page after processing the login
+    if(from.path === '/' &&to.fullPath==='/' &&to.fullPath==='/'){
+        const addrObj = await getAddr()
+        if(addrObj.code != '404'){
+          useDesktopStoreWidthOut().setShowFirstPage(false)
+          useDesktopStoreWidthOut().setUrl("http://"+addrObj.data+"/nc/")
+          next('/app');
+        }else{
+            next('/firstPage');
+            useDesktopStoreWidthOut().setShowFirstPage(true)
+        }
+      return
+    }else{
+        next()
+    }
+  });
+}
