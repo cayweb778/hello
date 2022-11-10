9207e9e50f9a60b9d87386bd1d1ea50fdd760e44
{cheng} <321841642@qq.com>
2022-11-11 00:00:03 +0800

    `reform(reform dir): reform dir`

--- webs/packages/boozsoft-platform-gdzc/src/views/boozsoft/stock/stock-caigou-dh/print/funs/jspdfFuns.ts
+++
@@ -1,7 +0,0 @@
-export function createPrintParams({width,height}){
-  if( width > height){
-    return ['l', 'mm',[width, height], false]
-  }else{
-    return ['p', 'mm',[width, height], false]
-  }
-}
--- webs/packages/boozsoft-platform-gdzc/src/views/boozsoft/stock/stock-caigou-dh/print/layouts/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="printSettingLayout" style="padding: 10px">
-    <slot></slot>
-  </div>
-</template>
-<script setup>
-import Header from './header/index.vue'
-</script>
-<style></style>
