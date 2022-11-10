ca3e6d987f582d80dc0181e4babca3c638b444e4
{cheng} <321841642@qq.com>
2022-11-11 00:13:32 +0800

    `reform(reform dir): reform dir`

--- webs/packages/boozsoft-cdn/pages/rbac/menuData.ts
+++
@@ -1,6 +0,0 @@
-export const modules = import.meta.globEager('../../../../pages/**/*.ts')
-
-const menus = Object.values(modules).map(it => it.menus)
-
-
-export const sysMenuQueryAll = () => JSON.parse(JSON.stringify({menus:menus.flatMap(it=>it), modules}));
