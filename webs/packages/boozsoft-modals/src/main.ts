4dc061c1699fe8f5c99375a7f34c2f308b081483
{cheng} <321841642@qq.com>
2022-11-18 16:56:33 +0800

    wip(ncmodals): ncmodals

--- webs/packages/boozsoft-modals/src/main.ts
+++ webs/packages/boozsoft-modals/src/main.ts
@@ -1,15 +1,13 @@
 import './public-path'
-import {useUserStoreWidthOut} from '/@/store/modules/user';
-import {NcLoader, goPdf, goApp} from '/@/utils/preHandle';
-import {createApp} from "vue";
-import App from "/@/boozsoft/App.vue";
+import { goApp} from '/@/utils/preHandle';
 
 // check api
 async function runApp(){
-    NcLoader()
-      .thenGoPdf(async () => await goPdf())
-      // sso返回信息
-      // 没登陆登陆
-      .thenGoApp(async () => !useUserStoreWidthOut().isSsoBack() && await goApp())
+  await goApp()
+    // NcLoader()
+    //   .thenGoPdf(async () => await goPdf())
+    //   // sso返回信息
+    //   // 没登陆登陆
+    //   .thenGoApp(async () => !useUserStoreWidthOut().isSsoBack() && await goApp())
 }
 runApp()
