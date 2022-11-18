73b23950e2355f7f81bc9ed3d34bee50b65698b0
{cheng} <321841642@qq.com>
2022-11-18 23:11:48 +0800

    wip(ncmodals): ncmodals

--- webs/packages/boozsoft-platform-home/src/main.ts
+++ webs/packages/boozsoft-platform-home/src/main.ts
@@ -1,13 +1,6 @@
 console.log(window.localStorage)
 
-function aaaa(){
-  for (var i = 0; i < window.top.localStorage.length; i++) {
-    var key = window.top.localStorage.key(i); //获取本地存储的Key
-    console.log(window.top.localStorage.getItem(key))
-    window.localStorage.setItem(key,window.top.localStorage.getItem(key));//所有value
-  }
-}
-aaaa()
+
 import './public-path'
 import {useUserStoreWidthOut} from '/@/store/modules/user';
 import {NcLoader, goPdf, goApp} from '/@/utils/preHandle';
